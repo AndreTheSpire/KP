dcdb0def7bd57436d8b3d2868ee20b4d2dfc6c27
PatrickFPSGamer <patrick.kusanagi@yahoo.com>
2022-05-22 20:06:41 +0700

    Update layout_admin.blade.php

--- resources/views/layout/layout_admin.blade.php
+++ resources/views/layout/layout_admin.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <title>{{$title}} </title>
-    e
     <script src="https://cdn.tailwindcss.com/"></script>
 
   </head>
